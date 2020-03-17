ef0c2b2929e822e2b1b9b76468fdd5892fe7d1e9
cjhart11 <54540419+cjhart11@users.noreply.github.com>
2020-03-17 08:52:38 -0600

    Start of project files
    Contains assets for
    SteamVR
    First person camera
    Interacting with 3d objects

---
+++ Assets/Materials/Inventory.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public bool hasRed, hasBlue, hasGreen;
+    public bool hasRedDoor, hasBlueDoor, hasGreenDoor;
+
+}
---
+++ Assets/Materials/PoleManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoleManager : MonoBehaviour
+{
+    public enum Poles { redPole, bluePole, greenPole };
+    public Poles currentPole;
+}
